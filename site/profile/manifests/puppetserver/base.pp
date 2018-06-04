a48332afb3f75fb740e8cda75b4da636fd5cefc3
Jawlit Patel <jawlitkp@gmail.com>
2018-06-04 14:29:27 -0400

    dbeug

--- site/profile/manifests/puppetserver/base.pp
+++ site/profile/manifests/puppetserver/base.pp
@@ -1,6 +1,6 @@
 class profile::puppetserver::base {
   include r10k::mcollective
   class { 'r10k':
-      remote => 'https://github.com/jawlitkp/puppet-r10k.git',
+      remote => 'https://github.com/jawlitkp/puppet-control.git',
   }
 }
