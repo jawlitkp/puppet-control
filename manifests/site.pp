124bae0cd71f6f0544be6af3f853cb661c5a4276
Jawlit Patel <jawlitkp@gmail.com>
2018-06-05 11:41:00 -0400

    debug

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,6 @@
 node 'puppetmom.bedbath.com' {
   notify { "Catalog applying on puppetmom.bedbath.com.................": }
   include profile::puppetserver::masterofmasters
-  include profile::activemq::server
+#  include profile::activemq::server
   notify { "Catalog applied on puppetmom.bedbath.com.................": }
 }
