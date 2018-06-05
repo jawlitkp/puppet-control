09d2ac40558bd492ba15a800a3f7491aae59ad6b
Jawlit Patel <jawlitkp@gmail.com>
2018-06-05 11:20:49 -0400

    debug

--- site/profile/manifests/activemq/manifests/init.pp
+++ site/profile/manifests/activemq/manifests/init.pp
@@ -1,15 +1,22 @@
 class activemq {
 
+  package { 'puppet-release-repo':
+    ensure => latest,
+    provider => 'rpm',
+    source   => 'http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm',
+    before => Package[ "activemq" ]
+  }
+
   package { 'activemq':
     ensure => latest,
     before => Service[ "activemq" ]
   }
-  
+
   service { 'activemq':
     ensure => running,
     enable => true
   }
-  
+
   file { '/etc/activemq/activemq.xml':
     ensure => present,
     source => 'puppet:///modules/activemq/activemq.xml',
@@ -18,7 +25,7 @@ class activemq {
     mode   => '0640',
     notify => Service['activemq']
   }
-  
+
   file { 'activemq-link':
     path    => '/usr/share/activemq/activemq-data',
     ensure  => link,
